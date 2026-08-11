801186281598aa732efd36a34c949afb1cad43c9
yujin5959 <jinjin17104@naver.com>
2026-08-11 18:35:07 +0900

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindCss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindCss()],
-  base: "/yujin5959.github.io/",
+  base: "/",
 });
